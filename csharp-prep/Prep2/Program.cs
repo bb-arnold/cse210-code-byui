aca15c419a8c8e5f56e0ec8d97f4b9a5174c6588
bb-arnold <bridger.arnold@yahoo.com>
2026-09-18 12:23:28 -0600

    Prep 2 added grade calculation program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentageString = Console.ReadLine();
+
+        int gradePercentage = int.Parse(gradePercentageString);
+
+        string letter = "";
+        string sign = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";    
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";    
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";    
+        }
+        else
+        {
+            letter = "F";    
+        }
+
+        if ((gradePercentage % 10) >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if ((gradePercentage % 10) < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($" Your grade is {letter + sign}");
+
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congradulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Keep trying!");
+        }
+ 
     }
 }
