ff5aff7ce8bb319e35d0dfbad07581716f979ad8
bb-arnold <bridger.arnold@yahoo.com>
2026-09-18 13:09:10 -0600

    Prep 3 added number guessing program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        //Create variables
+         int gNumber = -1;
+         int rNumber = randomGenerator.Next(1,101);
+         int i = 0;
+         string playAgain = "";
+
+        do
+        {
+            //Ask for your guess and change it into an int.
+            //Saved as gNumber.
+            Console.Write("What is your guess? ");
+            string gNumberString = Console.ReadLine();
+            gNumber = int.Parse(gNumberString);
+
+            //Keep track of the number of guesses
+            i++;
+
+            //Check values against each other, print appropriate
+            //messages.
+            if (gNumber < rNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (gNumber > rNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! It took you {i} guess(es)");
+
+                //See if you want to play again
+                Console.Write("Would you like to play again? (yes or no) ");
+                playAgain = Console.ReadLine();
+
+                //Reset for next game
+                i = 0;
+                rNumber = randomGenerator.Next(1,101);
+
+            }
+
+
+        } while (playAgain != "no");
+
     }
 }
