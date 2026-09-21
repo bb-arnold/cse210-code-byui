30fb59c117cb3d3db7ae5ad49d02aeb4145dc9d3
bb-arnold <bridger.arnold@yahoo.com>
2026-09-21 11:32:17 -0600

    Finished testing and using Job class W02.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,10 +9,18 @@ class Program
         Job job1 = new Job();
         Job job2 =  new Job();
 
-        job1._company = "Chase Is awesome";
-        job2._company = "bananas";
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2005;
+        job1._endYear = 2025;
 
-        Console.WriteLine($"{job1._company}, {job2._company}");
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2025;
+        job2._endYear = 2026;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
         
     }
 }
