437da8931c808d8abe705452c7bb9aa68057bdc4
bb-arnold <bridger.arnold@yahoo.com>
2026-09-21 10:44:11 -0600

    Added Job class testing code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Init new objects, set their variables
+        Job job1 = new Job();
+        Job job2 =  new Job();
+
+        job1._company = "Chase Is awesome";
+        job2._company = "bananas";
+
+        Console.WriteLine($"{job1._company}, {job2._company}");
+        
     }
 }
