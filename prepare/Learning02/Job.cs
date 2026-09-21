6d4634b88b30eb14f5164c65c1da73426e3ab7bf
bb-arnold <bridger.arnold@yahoo.com>
2026-09-21 10:43:56 -0600

    Added Job class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Job
+{
+    //Class: Job
+        //Responsibilities:
+            //Keeps track of the company, job title, start year, and end year.
+
+        //Behaviors:
+            //Displays the job information in the format "Job Title (Company) StartYear-EndYear", for example: "Software Engineer (Microsoft) 2019-2022".
+
+    
+    //Initializer method
+    public Job()
+    {
+        
+    }
+
+    //Initial variables
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+}
